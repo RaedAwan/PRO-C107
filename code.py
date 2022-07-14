d3d9f7d7c38f6b7f2662df04bf250621415ebca4
RaedAwan <75720005+RaedAwan@users.noreply.github.com>
2022-07-14 13:24:26 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+df = pd.read_csv("data.csv")
+
+print(df.groupby("level")["attempt"].mean())
+
+
+graph = go.Figure(go.Bar(
+    x = df.groupby("level")["attempt"].mean(),
+    y = ['Level 1' , 'Level 2' , 'Level 3' , 'Level 4' ],
+    orientation='h'
+))
+
+graph.show()
+
+
+
+
+
+
+
+
